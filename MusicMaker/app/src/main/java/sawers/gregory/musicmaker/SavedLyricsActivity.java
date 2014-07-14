2c9f86e0fab0efbda1e089303a14136c660c4d72
gsawers <gregory.sawers@mindbodyonline.com>
2014-07-14 09:19:47 -0700

    App's done :D

--- MusicMaker/app/src/main/java/sawers/gregory/musicmaker/SavedLyricsActivity.java
+++ MusicMaker/app/src/main/java/sawers/gregory/musicmaker/SavedLyricsActivity.java
@@ -87,7 +87,8 @@ public class SavedLyricsActivity extends Activity
         if (position == 0) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
-        } else if (position == 1){
+        }
+        else if (position == 1){
             Intent intent = new Intent(this, LyricWriter.class);
             startActivity(intent);
         }
@@ -118,9 +119,6 @@ public class SavedLyricsActivity extends Activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
             getMenuInflater().inflate(R.menu.saved_lyrics, menu);
             restoreActionBar();
             return true;
@@ -130,9 +128,6 @@ public class SavedLyricsActivity extends Activity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
